7b6715ebc0bf904e359e6ab6bdbc079e83ecd772
hippy935 <hippy935@gmail.com>
2025-10-03 15:31:16 +0700

    huge update

---
+++ script.js
@@ -0,0 +1,11 @@
+const my_input = document.getElementById("myinput")
+const teext = document.getElementById('cl')
+
+function add(x) { 
+	my_input.value = my_input.value + x
+}
+
+function show_img() {
+    teext.style.color = 'red'
+
+}
